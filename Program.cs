c9d341c14132beba834ba3a36f4e90c2a22a27c0
Kab-35-16 <Kab-35-16@KAB-31-7->
2025-05-22 11:01:42 +0300

    2-task

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace _43PR
     {
         static void Main(string[] args)
         {
+             Notebook notebook = new Notebook("Lenovo", "80RJ", 300);
+             notebook.Output();
+             Console.ReadKey();
         }
     }
 }

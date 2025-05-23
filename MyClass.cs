fd71e5f2fccc8880236f38d77568fbd283d2082d
XIXpsix <lebedevdima902@gmail.com>
2025-05-23 11:39:46 +0300

    Update MyClass.cs

--- MyClass.cs
+++ MyClass.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,6 @@ namespace _43PR
 {
     class MyClass
     {
-        public string change;
+        public string change;// класс открыт строком change
     }
 }
